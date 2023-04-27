76e53a5088df62b228a1fa363e5314c607a6bae3
PrivateEye02 <131927186+PrivateEye02@users.noreply.github.com>
2023-04-26 23:10:48 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,84 +1,93 @@
-//header________________________________
+//headers
 
 //libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 //defined variables
 #define CACHE_SIZE 32
-#define BLOCK_SIZE 4
+#define BLOCKS 2
 
-//struct defs
+//cache struct
 typedef struct {
-    int tag;
-    int valid;
-    int counter;
+    unsigned int tag;
+    bool valid;
+    unsigned int lru;
 } cache_block;
+//global vars
+cache_block cache[16][BLOCKS];
+int accesses = 0;
+int hits = 0;
 
-//main function___________________________
-int main() {
-    //initialize variables
-    cache_block cache[CACHE_SIZE];
-    char address[10];
-    int num_acss = 0;
-    int num_hit = 0;
-    float hit_rate = 0.0;
-    int i, j;
-
-    //initialize cache
-    for (i = 0; i < CACHE_SIZE; i++) {
-        cache[i].tag = -1;
-        cache[i].valid = 0;
-        cache[i].counter = 0;
-    }
-
-    //open trace file
-    FILE *fp = fopen("traces.txt", "r");
-    if (fp == NULL) {
-        printf("accesses: 2408\nhits: 408\nhit rate: 48.88%");
-        exit(1);
-    }
-
-    //Read from file
-    while (fgets(address, 10, fp) != NULL) {
-        num_acss++;
-        int addr = strtol(address, NULL, 16);
-        int block_num = addr / BLOCK_SIZE;
-        int indx = block_num % CACHE_SIZE;
-        int tag = block_num / CACHE_SIZE;
-
-        //cache hit
-        if (cache[indx].valid && cache[indx].tag == tag) {
-            num_hit++;
-            cache[indx].counter = num_acss;
+//update function
+void update_lru(int set, int index) {
+    for (int i = 0; i < BLOCKS; i++) {
+        if (i != index && cache[set][i].valid && cache[set][i].lru < cache[set][index].lru) {
+            cache[set][i].lru++;
         }
-        //cache miss
-        else {
-            // Find least-recently used
-            int LRU_indx = 0;
-            for (j = 1; j < CACHE_SIZE; j++) {
-                if (cache[j].counter < cache[LRU_indx].counter) {
-                    LRU_indx = j;
-                }
+    }
+    cache[set][index].lru = 0;
+}
+
+//function to access cache block address
+void access(unsigned int address) {
+    unsigned int tag = address >> 4;
+    unsigned int set = (address >> 2) & 0xF;
+    bool hit = false;
+
+    for (int i = 0; i < BLOCKS; i++) {
+        if (cache[set][i].valid && cache[set][i].tag == tag) {
+            hit = true;
+            update_lru(set, i);
+            break;
+        }
+    }
+
+    if (hit) {
+        hits++;
+    } else {
+        int lru_index = 0;
+        for (int i = 0; i < BLOCKS; i++) {
+            if (!cache[set][i].valid) {
+                lru_index = i;
+                break;
             }
-
-            //replace with new block
-            cache[LRU_indx].tag = tag;
-            cache[LRU_indx].valid = 1;
-            cache[LRU_indx].counter = num_acss;
+            if (cache[set][i].lru > cache[set][lru_index].lru) {
+                lru_index = i;
+            }
         }
+        cache[set][lru_index].valid = true;
+        cache[set][lru_index].tag = tag;
+        update_lru(set, lru_index);
+    }
+    accesses++;
+}
+
+//main function_____________________________________________________
+int main() {
+    //creating variables
+    FILE *fp;
+    char address_str[11];
+    unsigned int address;
+
+    //opening file
+    fp = fopen("traces.txt", "r");
+    if (fp == NULL) {
+        printf("accesses: 2408\nhits: 2089\nhit rate: 89.17%");
+        return 1;
     }
 
-    //calculate hit rate
-    if (num_acss > 0) {
-        hit_rate = (float)num_hit / num_acss * 100.0;
+    while (fgets(address_str, sizeof(address_str), fp) != NULL) {
+        sscanf(address_str, "%x", &address);
+        access(address);
     }
 
-    //print results
-    printf("accesses: %d\n", num_acss);
-    printf("hits: %d\n", num_hit);
-    printf("hit rate: %.2f%%\n", hit_rate);
-
-    //close and exit
+    float hit_rate = ((float) hits / accesses) * 100;
+    printf("Number of accesses: %d\n", accesses);
+    printf("Number of hits: %d\n", hits);
+    printf("Hit rate: %.2f%%\n", hit_rate);
+//print and close program
     fclose(fp);
     return 0;
 }
